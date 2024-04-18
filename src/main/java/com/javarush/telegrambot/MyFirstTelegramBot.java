65d1f75095be86cefde456dc4f0e311d1dbefbfd
n-tolegenov <nurbol.tolegenovv@gmail.com>
2024-04-18 17:17:00 +0600

    Добавлена отправка картинок в Telegram-боте

--- src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/com/javarush/telegrambot/MyFirstTelegramBot.java
@@ -26,11 +26,13 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         // Отобразим сообщение о начале игры - нужно взломать холодильник
         if(getMessageText().equals("/start")){
             addUserGlory(0);
+            sendPhotoMessageAsync("step_1_pic");
             sendTextMessageAsync(STEP_1_TEXT, Map.of("Взлом холодильника!", "step_1_btn"));
         }
 
         if(getCallbackQueryButtonKey().equals("step_1_btn")){
             addUserGlory(20);
+            sendPhotoMessageAsync("step_2_pic");
             sendTextMessageAsync(STEP_2_TEXT,
                     Map.of( "Взять сосиску! +20 славы", "step_2_btn",
                             "Взять рыбку! +20 славы", "step_2_btn",
@@ -40,11 +42,13 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         // Взламываем робот пылесос
         if(getCallbackQueryButtonKey().equals("step_2_btn")){
             addUserGlory(20);
+            sendPhotoMessageAsync("step_3_pic");
             sendTextMessageAsync(STEP_3_TEXT, Map.of("Взлом робота-пылесоса!", "step_3_btn"));
         }
 
         if(getCallbackQueryButtonKey().equals("step_3_btn")){
             addUserGlory(30);
+            sendPhotoMessageAsync("step_4_pic");
             sendTextMessageAsync(STEP_4_TEXT,
                     Map.of( "Отправить робот пылесос за едой! +30 славы", "step_4_btn",
                             "Покататься на роботе пылесосе! +30 славы", "step_4_btn",
@@ -54,35 +58,44 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
         // Взламываем камеру Go-Pro
         if(getCallbackQueryButtonKey().equals("step_4_btn")){
             addUserGlory(30);
-            sendTextMessageAsync(STEP_5_TEXT, Map.of("Взлом камеры Go-Pro!", "step_5_btn"));
+            sendPhotoMessageAsync("step_5_pic");
+            sendTextMessageAsync(STEP_5_TEXT, Map.of("Включить и одеть камеру Go-Pro!", "step_5_btn"));
         }
 
         if(getCallbackQueryButtonKey().equals("step_5_btn")){
             addUserGlory(40);
+            sendPhotoMessageAsync("step_6_pic");
             sendTextMessageAsync(STEP_6_TEXT,
-                    Map.of( "Включить камеру и начать запись! +40 славы", "step_6_btn",
-                            "Стереть карту памяти! +40 славы", "step_6_btn",
-                            "Сбросить все настройки! +40 славы", "step_6_btn"));
+                    Map.of( "Бегать по крышам и снимать все подряд! +40 славы", "step_6_btn",
+                            "Нападать на кошек и снимать это на видео! +40 славы", "step_6_btn",
+                            "Нападать из засады на собак! +40 славы", "step_6_btn"));
         }
 
         // Взламываем компьютер
         if(getCallbackQueryButtonKey().equals("step_6_btn")){
             addUserGlory(40);
+            sendPhotoMessageAsync("step_7_pic");
             sendTextMessageAsync(STEP_7_TEXT, Map.of("Взлом компьютера!", "step_7_btn"));
         }
 
         if(getCallbackQueryButtonKey().equals("step_7_btn")){
             addUserGlory(50);
+            sendPhotoMessageAsync("step_8_pic");
             sendTextMessageAsync(STEP_8_TEXT,
-                    Map.of( "Начать кодить в редакторе VSCode! +50 славы", "step_8_btn",
-                            "Открыть браузер и проходить курсы по Java! +50 славы", "step_8_btn",
-                            "Делать презентацию в Power Point! +50 славы", "step_8_btn"));
+                    Map.of( "Выйти во двор и похвастаться! +50 славы", "step_8_btn"));
         }
 
         // Хвастаемся дворовым котам
         if(getCallbackQueryButtonKey().equals("step_8_btn")){
             addUserGlory(50);
-            sendTextMessageAsync(FINAL_TEXT + "\n Накоплено: " + getUserGlory() + " славы", Map.of("Выйти во двор!", "step_9_btn"));
+//            sendPhotoMessageAsync("final_pic");
+            sendImageMessageAsync(
+                    "C:\\Users\\Nurbol\\Desktop\\TelegramBot\\src\\main\\resources\\images\\final_pic.jpg");
+            sendTextMessageAsync(FINAL_TEXT);
+        }
+
+        if(getMessageText().equals("/glory")){
+            sendTextMessageAsync("Накопленный количество очков славы : " + getUserGlory());
         }
 
     }
